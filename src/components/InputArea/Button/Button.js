d393f1672d14eb11b416a6223b00063f209f098f
Joanna Makarewicz <joanna.milun@gmail.com>
2023-06-19 13:10:06 +0100

    Adding Airtable connection

--- src/components/InputArea/Button/Button.js
+++ src/components/InputArea/Button/Button.js
@@ -2,11 +2,29 @@ import React, { useContext } from "react";
 import { BsPlus } from "react-icons/bs";
 import "../Button/Button.css";
 import NewInputContext from "../../../context/NewInputContext";
+import axiosInstance  from '../../../services/config';
 
 const Button = () => {
   const newValues = useContext(NewInputContext);
-  const saveInfo = () => {
-    console.log(newValues.input, newValues.chosenCategory);
+
+
+  const saveInfo = async () => {
+    try {
+      const res = await axiosInstance.post("/expenses", {
+        records: [
+          {
+            fields: {
+              name: newValues.input,
+              category: newValues.chosenCategory,
+              price:12,
+            },
+          },
+        ],
+      });
+console.log('poszło');
+    } catch (ex) {
+      console.log(ex.response);
+    }
   };
   return (
     <NewInputContext.Consumer>
